9fa4494032519030beb40017ddf58fa8138c7a5e
kateyatsk <klaettoe2023@gmail.com>
2025-06-10 16:38:33 +0300

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Екатерина Яцкевич on 10.06.25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
